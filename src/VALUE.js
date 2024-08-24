0507280b2dff6fe53208759365c39e6e042daf29
Arya Adhikari <98677324+AryaAdhikari09@users.noreply.github.com>
2024-08-24 15:42:09 +0530

    asdasd

--- src/VALUE.js
+++ src/VALUE.js
@@ -1,2 +1,3 @@
-// export const backendIp = 'https://sld-test.onrender.com';
-export const backendIp = 'http://127.0.0.1:5000';
+// export const backendIp = 'http://127.0.0.1:5000';
+export const backendIp = 'https://sankalp-server-r22uekd9u-aryas-projects-6ac239bc.vercel.app';
+
