97363facf7cb629070f21dcb30336dc6cb832212
Arya Adhikari <98677324+AryaAdhikari09@users.noreply.github.com>
2024-08-24 17:39:26 +0530

    dfervr

--- src/VALUE.js
+++ src/VALUE.js
@@ -1,3 +1,3 @@
 // export const backendIp = 'https://sld-test.onrender.com';
-export const backendIp = 'https://sankalp-server.onrender.com/';
+export const backendIp = 'https://sankalp-server.onrender.com';
 // export const backendIp = 'http://localhost:5000';
