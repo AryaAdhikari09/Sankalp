b160dc5304401c466552ff2266fd764df4c80e00
Arya Adhikari <98677324+AryaAdhikari09@users.noreply.github.com>
2024-08-24 17:31:05 +0530

    jhgfdszzxcvbnm,

--- src/VALUE.js
+++ src/VALUE.js
@@ -1,2 +1,3 @@
 // export const backendIp = 'https://sld-test.onrender.com';
-export const backendIp = 'http://localhost:5000';
+export const backendIp = 'https://sankalp-server.onrender.com/';
+// export const backendIp = 'http://localhost:5000';
