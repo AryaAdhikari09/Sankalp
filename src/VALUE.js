1c14b7e86ee1cb91ff714d1fc59a6ef678d1c4bb
Arya Adhikari <98677324+AryaAdhikari09@users.noreply.github.com>
2024-08-24 16:16:26 +0530

    sdffsdfasda

--- src/VALUE.js
+++ src/VALUE.js
@@ -1,3 +1,3 @@
 // export const backendIp = 'http://127.0.0.1:5000';
-export const backendIp = 'https://sankalp-server-r22uekd9u-aryas-projects-6ac239bc.vercel.app';
+export const backendIp = 'https://sankalp-server-7wyzu6d6s-aryas-projects-6ac239bc.vercel.app/';
 
